f69dd12254fad823a08891b43675c6da8b90b5e6
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-23 04:11:28 +0500

    Addding word counter

---
+++ word.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const userAns = await inquirer.prompt([
+    {
+        type: "input",
+        name: "sentence",
+        message: "Write your sentence to count the words?"
+    }
+]);
+console.log(chalk.bgBlueBright("***Wellcome to Our Word Counter***"));
+const words = userAns.sentence.trim().split(" ");
+console.log(chalk.italic.magentaBright(`Your sentense words are:  ${words}`));
+console.log(chalk.italic.magentaBright(`Your sentence word count is: ${words.length}`));
+console.log(chalk.bgBlueBright("***Thankyou for using our Word counter***"));
